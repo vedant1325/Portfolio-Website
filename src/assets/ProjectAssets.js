fe5a7b8adc1fe1f6df8d15dccd7b6797800c433c
vedant1325 <147478924+vedant1325@users.noreply.github.com>
2025-03-04 16:27:20 +0530

    Update ProjectAssets.js

--- src/assets/ProjectAssets.js
+++ src/assets/ProjectAssets.js
@@ -4,6 +4,8 @@ import pro_backgroung3 from "./pro_background3.png"
 import pro_backgroung4 from "./pro_background4.png"
 import pro_backgroung5 from "./pro_background5.png"
 import pro_backgroung6 from "./pro_background6.png"
+import pro_backgroung6 from "./Estate.png"
+import pro_backgroung6 from "./Imagify.png"
 
 export const proAssets = {
     pro_backgroung1,
@@ -11,27 +13,30 @@ export const proAssets = {
     pro_backgroung3,
     pro_backgroung4,
     pro_backgroung5,
-    pro_backgroung6
+    pro_backgroung6,
+    Imagify,
+    Estate
+    
 
 }
 export const proInfo = [
 
     {
 
-        title: "Amazon Clone",
-        pro_image: pro_backgroung1,
-        desc: "This project served as my initiation into front-end development and provided valuable insights into structuring and styling web pages."
-    },
-    {
-        title: "Sotials Restaurant Website",
-        pro_image: pro_backgroung2,
-        desc: "It's a project encompassing all fundamental concepts of HTML, CSS, JS, and PHP, seamlessly integrating MySQL for database connectivity"
+        title: "Real Estate Website",
+        pro_image: Estate,
+        desc: "A modern real estate platform built with the MERN (MongoDB, Express.js, React.js, Node.js) stack.Users can browse property listings, log in securely, and subscribe for real estate updates."
     },
     {
         title: "Weather App",
         pro_image: pro_backgroung3,
         desc: "It is a weather application that fetches weather data from the OpenWeatherMap API based on user-provided locations. We’ll then display the weather information in a user-friendly manner."
 
+    },
+    {
+        title: "Imagify - AI Image Generator ✨🎨",
+        pro_image: Imagify,
+        desc: "🚀 Imagify is a MERN stack web app that allows users to generate AI images using the Clipdrop API.🔒 Users need to create an account to access the AI image generation feature."
     },
     {
         title: "Gemini Clone",
